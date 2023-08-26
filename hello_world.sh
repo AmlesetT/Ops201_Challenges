edd53ee835cb880944ac51b462a5c440bef52369
Amleset Tesfamariam <139098353+AmlesetT@users.noreply.github.com>
2023-08-26 07:04:36 +0000

    Initial commit

---
+++ hello_world.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Script Name:                  My First Bash Script
+# Author:                       Amleset Tesfamariam
+# Date of latest revision:      08/25/2023
+# Purpose:                      To create a script that will print "hello world" to the screen.
+
+# Declaration of variables
+
+
+
+# Main
+echo "Hello and Goodbye World!"
+
+
+# End
